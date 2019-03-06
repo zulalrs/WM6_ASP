b20752ab4c844f02dc8cdd00e7a41f4632e192b2
zulalrs <zulal.r.s@gmail.com>
2019-03-06 11:41:22 +0300

    Entity+repository

---
+++ Rabbit.BLL/Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using Rabbit.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.BLL.Repository
+{
+    public class CustomerRepo : RepositoryBase<Customer, Guid> { }
+    public class MailLogRepo : RepositoryBase<MailLog, Guid> { }
+}
---
+++ Rabbit.DAL/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using Rabbit.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.DAL
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("name=MyCon")
+        {
+            this.InstanceDate = DateTime.Now;
+        }
+        public DateTime InstanceDate { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<MailLog> MailLogs { get; set; }
+    }
+}
