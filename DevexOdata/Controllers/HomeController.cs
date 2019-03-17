f53084b8093889ccbb928de0ea116d74b073c239
Zülal Rabia Savrum <zulal.r.s@gmail.com>
2019-03-17 23:19:54 +0300

    fake data

--- DevexOdata/Controllers/HomeController.cs
+++ DevexOdata/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevexOdata.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +14,27 @@ namespace DevexOdata.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About() // About sekmesine tıkladığımızda veri üretecek. Sadece about viewın ekranını gösterecek, ekstra birşey göstermeyecek .
         {
             ViewBag.Message = "Your application description page.";
+
+            ApplicationDbContext db = new ApplicationDbContext(); // Context nesnemize eriştik.
+            for (int i = 0; i < 5000; i++) 
+            {
+                db.Customers.Add(new Customer() // Customer tablomuza, fake data kullanarak Costumer tipinde yeni nesneler ekledik.
+                {
+                    // Propertyleri doldurduk. 
+                    Name = FakeData.NameData.GetFirstName(),
+                    Address = FakeData.PlaceData.GetAddress(),
+                    Balance = FakeData.NumberData.GetNumber(1250, 99999),
+                    Phone = "05" + FakeData.PhoneNumberData.GetPhoneNumber().Replace("-", "").Substring(0, 10),
+                    Surname = FakeData.NameData.GetSurname()
+                });
+                if (i % 100 == 0) // Tek bir seferde 5000 tane ekleyemiyor onun için her 100 taneden sonra dbsavechanges diyerek ekleme yapmış oluyoruz.
+                    db.SaveChanges();
+            }
+
+            db.SaveChanges();
 
             return View();
         }
---
+++ DevexOdata/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DevexOdata.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
